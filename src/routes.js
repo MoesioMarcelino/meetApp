0e181955af0295721d0bc094bd4403c6195f7148
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-07-17 12:05:19 -0300

    create sessions

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
+
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
