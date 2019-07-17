430a3697d3b69644203ac1ac30d3461261b1815a
MoesioMarcelino <moesiomarcelino1@gmail.com>
2019-07-17 17:31:02 -0300

    create meetups

---
+++ src/app/controllers/MeetupController.js
@@ -0,0 +1,9 @@
+import MeetUp from '../models/Meetup';
+
+class MeetupController {
+  async store(req, res) {
+    return res.json();
+  }
+}
+
+export default new MeetupController();
---
+++ src/app/models/Meetup.js
@@ -0,0 +1,29 @@
+// id, title, description, location, date, user_id, banner_id, created_id, updated_id
+
+import Sequelize, { Model } from 'sequelize';
+
+class Meetup extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        title: Sequelize.STRING,
+        description: Sequelize.STRING,
+        location: Sequelize.STRING,
+        date: Sequelize.DATE,
+        user_id: Sequelize.INTEGER,
+        banner_id: Sequelize.INTEGER,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'banner_id', as: 'banner' });
+  }
+}
+
+export default Meetup;
